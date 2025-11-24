5593ac5dc781b5d8385d8814b409d3f5235bdf60
DanielSantana1234 <danielsantana01189@gmail.com>
2025-11-24 00:11:32 -0500

    Main Menu implementation

--- Wander/src/scenes/menuScene.cpp
+++ Wander/src/scenes/menuScene.cpp
@@ -1,4 +1,4 @@
-#include "menuScene.h"
+﻿#include "menuScene.h"
 
 std::string printHello() {
 	return "Hello from menuScene!";
@@ -9,28 +9,68 @@ void strt_menu() {
 	int monitorWidth = GetMonitorWidth(currentMonitor);
 	int monitorHeight = GetMonitorHeight(currentMonitor);
 
-	InitWindow(monitorWidth, monitorHeight, "Menu Scene");
+	// InitWindow(800, 450, "Wander");
+	InitWindow(monitorWidth, monitorHeight, "Wander");
 	ToggleFullscreen();
 
-	bool isFullscreen = true;
+	bool isFullscreen = false;
+
+	// For the height as well as width of the game art
+	Texture2D background = LoadTexture("C:\\Users\\danie\\Downloads\\Wander\\Wander\\assets\\sprites\\Sprite-0001.png");
+
+	// Creating a render texture at the pixel art resolution
+	// RenderTexture2D target = LoadRenderTexture(gameWidth, gameHeight);
+	// SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
+
+	// Image image = LoadImage("C:\\Users\\danie\\Downloads\\Wander\\Wander\\assets\\sprites\\Sprite-0001.png");
+	// Texture2D background = LoadTexture("C:\\Users\\danie\\Downloads\\Wander\\Wander\\assets\\sprites\\Sprite-0001.png");
+	if (background.id == 0) {
+		std::cerr << "error while trying to load the texture!" << std::endl;
+	}
+	SetTextureFilter(background, TEXTURE_FILTER_POINT);
+
+	int gameWidth = background.width;
+	int gameHeight = background.height;
+	
+
+	SetTargetFPS(60);
+
+	std::cout << "Monitor Width: " << monitorWidth << "\n";
+	std::cout << "Monitor Height: " << monitorHeight << "\n";
+
 	while (!WindowShouldClose()) {
 		if (IsKeyPressed(KEY_F)) {
 			if (!isFullscreen) {
-				int monitorWidth = GetMonitorWidth(currentMonitor); // Why can this not be global?
-				int monitorHeight = GetMonitorHeight(currentMonitor);
-				SetWindowSize(monitorWidth, monitorHeight);
-				ToggleFullscreen();
-				isFullscreen = true;
-			}
-			else {
 				ToggleFullscreen();
 				SetWindowSize(800, 640);
-				isFullscreen = false;
 			}
+			else {
+				SetWindowSize(GetScreenWidth(), GetScreenHeight());
+				ToggleFullscreen();
+			}
+			isFullscreen = !isFullscreen;
 		}
+
+		/*BeginTextureMode(target);
+		ClearBackground(WHITE);
+		DrawTexture(target.texture, 0, 0, WHITE);
+		EndTextureMode();*/
+
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
+		ClearBackground(WHITE);
+		DrawTexturePro(
+			background,
+			Rectangle{ 0, 0, (float)gameWidth, (float)gameHeight}, // So the part of the texture to draw
+			Rectangle{ 0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()}, // So where to draw the texture on the screen
+			Vector2{ 0, 0 },
+			0.0f,
+			WHITE
+		);
+		//DrawTextureEx(background, { 0, 0 }, 0, 7, WHITE);
 		EndDrawing();
 	}
+
+	//UnloadRenderTexture(target);
+	UnloadTexture(background);
 	CloseWindow();
 }
